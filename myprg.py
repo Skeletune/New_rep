d3329bfba79e6e0c96576305518448d78fc8da0e
Якушенко Дмитрий <213@gmail.com>
2020-12-08 17:17:25 +0300

    Демо слияния

--- myprg.py
+++ myprg.py
@@ -1,7 +1,7 @@
 def my_superfunction():
     print('Спасите меня')
+    print('Демо для слияния')
 
 
 my_superfunction()
 print('Изменений нет... Наверн')
-print('Версия 2.3 в релизе! Купите ее за 99.99 рублей!')
